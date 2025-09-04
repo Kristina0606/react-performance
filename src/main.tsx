43feb496fa84c232ac8e0e46b156b399b7652b50
Kristina0606 <krisbubenka@icloud.com>
2025-09-04 22:14:33 +0300

    fix: fix base path

--- src/main.tsx
+++ src/main.tsx
@@ -9,33 +9,36 @@ import { store } from './store/store.ts';
 import TablePage from './pages/TablePage.tsx';
 import { ClipLoader } from 'react-spinners';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    HydrateFallback: () => (
-      <div className="h-screen flex items-center justify-center">
-        <ClipLoader
-          color="#57729dff"
-          loading={true}
-          size={48}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-      </div>
-    ),
-    children: [
-      {
-        index: true,
-        element: <MainPage />,
-      },
-      {
-        path: '/:country',
-        element: <TablePage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      HydrateFallback: () => (
+        <div className="h-screen flex items-center justify-center">
+          <ClipLoader
+            color="#57729dff"
+            loading={true}
+            size={48}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ),
+      children: [
+        {
+          index: true,
+          element: <MainPage />,
+        },
+        {
+          path: '/:country',
+          element: <TablePage />,
+        },
+      ],
+    },
+  ],
+  { basename: '/react-performance/' }
+);
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
