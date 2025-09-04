5d70433f684a409e0a3ddc5ff4cdec3033e09862
Kristina0606 <krisbubenka@icloud.com>
2025-09-04 21:57:37 +0300

    fix: fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rss-react-task1/',
+  base: '/react-performance/',
   build: {
     outDir: 'dist',
   },
